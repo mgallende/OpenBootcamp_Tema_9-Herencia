d5a27e3008c644d9962fd8312cd5eb1968317cce
mgallende <mgallende@mgallende.com>
2022-10-29 14:32:25 +0200

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //creamos el objeto persona en el main
-        Persona persona = new Persona();
-
-        //Asignamos valores a las propiedades con Set
-        persona.setNombre("Manuel Gómez");
-        persona.setEdad(62);
-        persona.setTelefono(683608805);
+        //creamos los objetos cliente y trabajador en el main
+        Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
 
         //imprimimos por consola
-        System.out.println("Nombre: " + persona.getNombre());
-        System.out.println("Edad: " + persona.getEdad());
-        System.out.println("Teléfono: " + persona.getTelefono());
+        System.out.println("Cliente: " + cliente.nombre + "Edad: " + cliente.edad + "Teléfono: " + cliente.telefono + "Crédito: " + cliente.credito);
 
+        System.out.println("\r\n");
+
+        System.out.println("Trabajador: " + trabajador.nombre + "Edad: " + trabajador.edad + "Teléfono: " + trabajador.telefono + "salario: " + trabajador.salario);
     }
 }
 
@@ -22,31 +19,15 @@ public class Main {
 // Creamos la clase Persona
 
 class Persona {
-    private String nombre;
-    private int    edad;
-    private int    telefono;
+    String nombre = "Manuel Gómez Allende";
+    int    edad = 62;
+    int    telefono = 683608805;
+}
 
-    // Get y Set de la propiedad Nombre
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+class Cliente extends Persona{
+    int credito = 2500;
+}
 
-    // Get y Set de la propiedad Edad
-    public int getEdad() {
-        return edad;
-    }
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    // Get y Set de la propiedad Teléfono
-    public int getTelefono() {
-        return telefono;
-    }
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
-    }
+class Trabajador extends Persona {
+    int salario = 32000;
 }
