5cc29e1aae7b8ed1457a6d37537447fe9792462d
mgallende <mgallende@mgallende.com>
2022-10-29 14:33:56 +0200

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-public class Main {
+public
+class Main {
 
-    public static void main(String[] args) {
+    public static
+    void main(String[] args) {
 
         //creamos los objetos cliente y trabajador en el main
         Cliente cliente = new Cliente();
         Trabajador trabajador = new Trabajador();
 
         //imprimimos por consola
-        System.out.println("Cliente: " + cliente.nombre + "Edad: " + cliente.edad + "Teléfono: " + cliente.telefono + "Crédito: " + cliente.credito);
+        System.out.println("Cliente: " + cliente.nombre + "Edad: " + cliente.edad
+                + "Teléfono: " + cliente.telefono + "Crédito: " + cliente.credito);
 
         System.out.println("\r\n");
 
-        System.out.println("Trabajador: " + trabajador.nombre + "Edad: " + trabajador.edad + "Teléfono: " + trabajador.telefono + "salario: " + trabajador.salario);
+        System.out.println("Trabajador: " + trabajador.nombre + "Edad: " + trabajador.edad
+                + "Teléfono: " + trabajador.telefono + "salario: " + trabajador.salario);
     }
 }
 
@@ -19,12 +23,12 @@ public class Main {
 // Creamos la clase Persona
 
 class Persona {
-    String nombre = "Manuel Gómez Allende";
-    int    edad = 62;
+    String nombre   = "Manuel Gómez Allende";
+    int    edad     = 62;
     int    telefono = 683608805;
 }
 
-class Cliente extends Persona{
+class Cliente extends Persona {
     int credito = 2500;
 }
 
