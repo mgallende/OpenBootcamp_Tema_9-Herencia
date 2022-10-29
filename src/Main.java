275b82516876cc110cefb08341d680c26b2ca254
mgallende <mgallende@mgallende.com>
2022-10-29 14:51:28 +0200

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,30 @@ class Main {
     public static
     void main(String[] args) {
 
-        //creamos los objetos cliente y trabajador en el main
+        //creamos los objetos Cliente y trabajador en el main
+
         Cliente cliente = new Cliente();
+
         Trabajador trabajador = new Trabajador();
 
+        //Asignamos valores a las variables de ambas clases
+        cliente.setNombre("Manuel Gómez");
+        cliente.setEdad(62);
+        cliente.setTelefono(683608805);
+        cliente.setCredito(5000);
+        trabajador.setNombre("Manuel Gómez");
+        trabajador.setEdad(62);
+        trabajador.setTelefono(683608805);
+        trabajador.setSalario(35000);
+
         //imprimimos por consola
-        System.out.println("Cliente: " + cliente.nombre + "Edad: " + cliente.edad
-                + "Teléfono: " + cliente.telefono + "Crédito: " + cliente.credito);
+        System.out.println("Cliente: " + cliente.getNombre() + " |  Edad: " + cliente.getEdad()
+                + " | Teléfono: " + cliente.getTelefono() + " | Crédito: " + cliente.getCredito());
 
         System.out.println("\r\n");
 
-        System.out.println("Trabajador: " + trabajador.nombre + "Edad: " + trabajador.edad
-                + "Teléfono: " + trabajador.telefono + "salario: " + trabajador.salario);
+        System.out.println("Trabajador: " + trabajador.getNombre() + " |  Edad: " + trabajador.getEdad()
+                + " |  Teléfono: " + trabajador.getTelefono() + " |  Salario: " + trabajador.getSalario());
     }
 }
 
@@ -23,15 +35,60 @@ class Main {
 // Creamos la clase Persona
 
 class Persona {
-    String nombre   = "Manuel Gómez Allende";
-    int    edad     = 62;
-    int    telefono = 683608805;
+    private String nombre;
+    private int    edad ;
+    private int    telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
+
+// Creamos la clase Cliente que hereda de Persona con extends y añadimos la propiedad crédito
 
 class Cliente extends Persona {
-    int credito = 2500;
+    private int credito;
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+
 }
 
+// Creamos la clase Trabajador que hereda de Persona con extends y añadimos la propiedad salario
 class Trabajador extends Persona {
-    int salario = 32000;
+    private int salario;
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
 }
