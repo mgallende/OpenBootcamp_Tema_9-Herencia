e6202d337c02489993535cd7b4efa1626ad5a986
mgallende <mgallende@mgallende.com>
2022-10-30 23:00:18 +0100

    Con modificadores Provate y sus cporrespondientes Setters y getters

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+class Main {
 
     public static void main(String[] args) {
 
@@ -9,44 +9,86 @@ public class Main {
         Trabajador trabajador = new Trabajador();
 
         //Asignamos valores a las propiedades de la clase Cliente
-        cliente.nombre = "Manuel Gómez";
-        cliente.edad = 62;
-        cliente.telefono = 683608805;
-        cliente.credito = 50150;
+        cliente.setNombre("Manuel Gómez");
+        cliente.setEdad(62);
+        cliente.setTelefono(683608805);
+        cliente.setCredito(50150);
 
         //Asignamos valores a las propiedades de la Clase Trabajador
-        trabajador.nombre = "Jorge López";
-        trabajador.edad = 45;
-        trabajador.telefono = 606606606;
-        trabajador.salario = 350150;
+        trabajador.setNombre("Jorge López");
+        trabajador.setEdad(45);
+        trabajador.setTelefono(606606606);
+        trabajador.setSalario(350150);
 
         //imprimimos por consola
-        System.out.println("Cliente: " + cliente.nombre + " |  Edad: " + cliente.edad
-                + " | Teléfono: " + cliente.telefono + " | Crédito: " + cliente.credito);
+        System.out.println("Cliente: " + cliente.getNombre() + " |  Edad: " + cliente.getEdad()
+                + " | Teléfono: " + cliente.getTelefono() + " | Crédito: " + cliente.getCredito());
 
         System.out.println("\r\n");
 
-        System.out.println("Trabajador: " + trabajador.nombre + " |  Edad: " + trabajador.edad
-                + " |  Teléfono: " + trabajador.telefono + " |  Salario: " + trabajador.salario);
+        System.out.println("Trabajador: " + trabajador.getNombre() + " |  Edad: " + trabajador.getEdad()
+                + " |  Teléfono: " + trabajador.getTelefono() + " |  Salario: " + trabajador.getSalario());
     }
 }
 
 
-// Creamos la clase Persona
+// Creamos la clase Persona con modificadores "private" y los correspondientes setters y Getters
 
 class Persona {
-    String nombre;
-    int    edad ;
-    int    telefono;
+    private String nombre;
+    private int    edad ;
+    private int    telefono;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
 
-// Creamos la clase Cliente que hereda de Persona con extends y añadimos la propiedad crédito
+// Creamos la clase Cliente que hereda de Persona con extends y añadimos la propiedad crédito, con modificador "private" y los correspondientes setters y Getters
 
 class Cliente extends Persona {
-    int credito;
+    private int credito;
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;}
+
 }
 
-// Creamos la clase Trabajador que hereda de Persona con extends y añadimos la propiedad salario
+// Creamos la clase Trabajador que hereda de Persona con extends y añadimos la propiedad salario, con modificador "private" y los correspondientes setters y Getters
 class Trabajador extends Persona {
-    int salario;
+    private int salario;
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
 }
